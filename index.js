29857a6904a0f53dbf634497ed71a0016ce26b99
lbsena10 <lucas.lbsena@gmail.com>
2023-12-10 21:00:27 -0300

    Inicio do projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+//Config JSON response
+app.use(express.json())
+
+//Solve cors
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+//Public folder for images
+app.use(express.static('public'))
+
+//Routes
+const UserRoutes = require('./routes/UserRoutes')
+app.use('/users', UserRoutes)
+
+app.listen(5000)
